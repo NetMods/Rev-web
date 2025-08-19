86a4b557a6a9b6d3767520867e0f8ef74f1626ca
aryankumar07 <ak2839426@gmail.com>
2025-08-20 01:17:52 +0530

    feat: fixed preload annimation
    the preload annimation was not showinf bug in production

--- app/_sections/preloader-section/index.jsx
+++ app/_sections/preloader-section/index.jsx
@@ -30,8 +30,8 @@ const PreloaderSection = ({ onAnimationComplete }) => {
         { opacity: 1, y: 0, duration: 1, ease: "power3.out" },
       )
       .to(".brand-name", {
-        scale: 54,
-        y: 195,
+        scale: 56,
+        y: 200,
         duration: 4,
         delay: 2,
         ease: "power3.out",
