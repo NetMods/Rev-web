5c8820a537c346bd8f39cbc847a7a7051a0146ae
aryankumar07 <ak2839426@gmail.com>
2025-08-21 00:07:39 +0530

    feat: improved preloading animation
    add a timer and made them more smooth

--- app/_sections/preloader-section/index.jsx
+++ app/_sections/preloader-section/index.jsx
@@ -8,6 +8,7 @@ const PreloaderSection = ({ onAnimationComplete }) => {
         onAnimationComplete();
       },
     });
+
     animationTimeline
       .fromTo(
         ".tagline-first",
@@ -29,22 +30,53 @@ const PreloaderSection = ({ onAnimationComplete }) => {
         { opacity: 0, y: 30 },
         { opacity: 1, y: 0, duration: 1, ease: "power3.out" },
       )
-      .to(".brand-name", {
-        scale: 57,
-        y: 205,
-        duration: 4,
-        delay: 2,
-        ease: "power3.out",
-      })
-      .set(
+      .to(
         ".tagline-first, .tagline-second, .tagline-third",
-        { opacity: 0 },
-        "<",
+        {
+          opacity: 0,
+          y: -20,
+          duration: 1,
+          stagger: 0.2,
+          ease: "power3.inOut",
+        },
+        "scaleAndCount",
+      )
+      .to(
+        ".brand-name",
+        {
+          scale: 65,
+          y: 205,
+          duration: 3,
+          delay: 1,
+          ease: "power3.out",
+        },
+        "scaleAndCount",
       );
+
+    animationTimeline.to(
+      ".timer",
+      {
+        innerText: 100,
+        opacity: 1,
+        duration: animationTimeline.duration(),
+        ease: "linear",
+        snap: { innerText: 1 },
+        onUpdate: function () {
+          const element = document.querySelector(".timer");
+          if (element) {
+            element.innerText = Math.floor(this.targets()[0].innerText);
+          }
+        },
+      },
+      0, // start at the very beginning of the timeline
+    );
   }, []);
 
   return (
     <div className="absolute inset-0 flex min-h-screen w-screen flex-col items-center justify-center gap-4 bg-white text-black">
+      <div className="timer absolute right-0 bottom-0 z-10 text-9xl text-red-800 opacity-0">
+        0
+      </div>
       <div className="brand-name flex w-full translate-y-[30px] scale-50 items-center justify-center text-center text-9xl font-bold opacity-0">
         REV
       </div>
